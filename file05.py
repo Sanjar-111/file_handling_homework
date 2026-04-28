476a2b84335edee0c0c7bab3cb39c27dd3df287f
Sanjar-111 <dagarovsanjar38@gmail.com>
2026-04-28 16:03:59 +0500

    solved

--- file05.py
+++ file05.py
@@ -1,10 +1,13 @@
+import string
 def main(data:str):
-    """
-    The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
-    Args:
-        data: str
-    Returns:
-        list: return answer
-    """
-    
-# Read data from file
+    a=open(data,"r")
+    n=a.read()
+    i=0
+    digit=string.digits
+    count=0
+    while len(n)>i:
+        if n[i] in digit:
+            count+=1
+        i+=1
+    return [count]+[len(n)-count]
+print(main("data/data05.txt"))
