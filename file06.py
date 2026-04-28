0cc05e4d5988c2274e20cf37357d08e4fd97740f
Sanjar-111 <dagarovsanjar38@gmail.com>
2026-04-28 16:09:27 +0500

    solved

--- file06.py
+++ file06.py
@@ -1,10 +1,9 @@
 def main(data:str):
-    """
-    The data is from the file. Find the each row length and return as list type.
-    Args:
-        data: str
-    Returns:
-        list: return answer
-    """
-    
-# Read data from file
+    a=open(data,"r")
+    n=a.read()
+    ls=n.split("\n")
+    m=[]
+    for i in ls:
+        m+=[len(i)]
+    return m
+print(main("data/data06.txt"))
