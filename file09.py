d6efe157ab4ad303b118083382bfad79b63a2398
Sanjar-111 <dagarovsanjar38@gmail.com>
2026-04-28 16:36:07 +0500

    solved

--- file09.py
+++ file09.py
@@ -1,10 +1,12 @@
+import string
 def main(data:str):
-    """
-    The data is from the file. Find the smallest of the numeric characters.
-    Args:
-        data: str
-    Returns:
-        int: return answer
-    """
-
-# Read data from file
+    a=open(data,"r")
+    n=a.read()
+    numbers=string.digits
+    digit=9
+    for i in n:
+        if i in numbers:
+            if digit>int(i):
+                digit=int(i)
+    return digit
+print(main("data/data09.txt"))
