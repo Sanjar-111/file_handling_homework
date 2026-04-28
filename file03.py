64bea58ffc3cd7f3eacee8a61ae386f7861ba4c8
Sanjar-111 <dagarovsanjar38@gmail.com>
2026-04-28 11:59:33 +0500

    solved

--- file03.py
+++ file03.py
@@ -1,3 +1,4 @@
+import string
 def main(data:str):
     """
     The data is from the file. Return the digits as a list type.
@@ -6,5 +7,17 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    a=open(data,"r")
+    n=a.read()
+    b=string.digits
+    digit=[]
+    for i in n:
+        j=0
+        while len(b)>j:
+            if b[j]==i:
+                digit+=[i]
+            j+=1
+    return digit
+print(main("data/data03.txt"))
 
 # Read data from file
